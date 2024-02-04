ef3a124b7898cb7f0bcbc1507d18f530d06a2957
Amalia Cicala <cicala.amalia@gmail.com>
2024-02-04 16:40:20 +0100

    💄 Add AppHeader style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,11 +6,30 @@
       <h4 class="font-weight-bold text-uppercase">Amalia Cicala Photography</h4>
 
       <div class="d-flex">
-        <v-list-item link :title="$t('menu.home')" :to="{ name: 'Home' }" />
+        <v-list-item
+          link
+          :aria-label="$t('menu.home')"
+          :to="{ name: 'Home' }"
+          width="150"
+          height="64"
+          class="d-flex justify-center text-uppercase text-button font-weight-medium"
+          active-class="active font-weight-bold"
+        >
+          {{ $t('menu.home') }}
+        </v-list-item>
 
         <v-menu>
           <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props" link :title="$t('menu.photoAlbums')" />
+            <v-list-item
+              v-bind="props"
+              link
+              :aria-label="$t('menu.photoAlbums')"
+              width="150"
+              class="d-flex justify-center text-uppercase text-button font-weight-medium"
+              active-class="active font-weight-bold"
+            >
+              {{ $t('menu.photoAlbums') }}
+            </v-list-item>
           </template>
 
           <v-list>
@@ -20,8 +39,27 @@
           </v-list>
         </v-menu>
 
-        <v-list-item link :title="$t('menu.contacts')" :to="{ name: 'Contacts' }" />
+        <v-list-item
+          link
+          :aria-label="$t('menu.contacts')"
+          :to="{ name: 'Contacts' }"
+          width="150"
+          class="d-flex justify-center text-uppercase text-button font-weight-medium"
+          active-class="active font-weight-bold"
+        >
+          {{ $t('menu.contacts') }}
+        </v-list-item>
       </div>
     </v-container>
   </v-app-bar>
 </template>
+
+<style lang="scss" scoped>
+.active {
+  text-decoration: underline !important;
+  text-underline-offset: 2px !important;
+}
+:deep(.v-list-item__overlay) {
+  opacity: 0 !important;
+}
+</style>
