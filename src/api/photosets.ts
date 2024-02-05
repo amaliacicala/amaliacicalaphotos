89fdd7bd0ff653006472ab0b7a6a6a707562eb80
Amalia Cicala <cicala.amalia@gmail.com>
2024-02-05 22:42:47 +0100

    🔐 Update photoset key

--- src/api/photosets.ts
+++ src/api/photosets.ts
@@ -5,13 +5,13 @@ export const fetchHomeImages = async () => {
     const res = await axios.get('https://api.flickr.com/services/rest', {
       params: {
         method: 'flickr.photosets.getPhotos',
-        api_key: import.meta.env.VITE_API_KEY,
-        photoset_id: import.meta.env.VITE_PHOTOSET,
+        api_key: import.meta.env.VITE_FLICKR_API_KEY,
+        photoset_id: import.meta.env.VITE_HOME_PHOTOSET,
         extras: 'url_c, url_o',
         page: 1,
         format: 'json',
         nojsoncallback: 1,
-        per_page: 30
+        per_page: 50
       }
     });
 
