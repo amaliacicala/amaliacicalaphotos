361e9ac8eb2b1d5023875035399a4ccf49e620a6
Amalia Cicala <cicala.amalia@gmail.com>
2024-02-06 00:49:43 +0100

    ✨ Create app footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-footer class="d-flex justify-center text-caption pt-14">
+      <p>© {{ new Date().getFullYear() }} by Amalia Cicala</p>
+      <span class="divider">|</span>
+      <p>cicala.amalia@gmail.com</p>
+      <span class="divider">|</span>
+      <p>Milan & Worldwide.</p>
+    </v-footer>
+  </v-container>
+</template>
+
+<style lang="scss" scoped>
+.divider {
+  margin: 0 10px;
+  color: rgb(var(--v-theme-primary));
+}
+</style>
