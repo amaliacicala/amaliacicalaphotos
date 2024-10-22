16d041390b8dea933d233cb495bc7726a4d3576c
Amalia Cicala <cicala.amalia@gmail.com>
2024-10-22 16:55:18 +0200

    💬 update page titles

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,32 +15,38 @@ const router = createRouter({
     {
       path: '/artist-management',
       name: 'ArtistManagement',
-      component: ArtistManagementView
+      component: ArtistManagementView,
+      meta: { title: 'Artist Management Services' }
     },
     {
       path: '/photos',
       name: 'HomePhotos',
-      component: HomeGallery
+      component: HomeGallery,
+      meta: { title: 'Concert and Music Photography' }
     },
     {
       path: '/photos/contacts',
       name: 'Contacts',
-      component: () => import('../views/photos/ContactsView.vue')
+      component: () => import('../views/photos/ContactsView.vue'),
+      meta: { title: 'Concert and Music Photography | Contacts' }
     },
     {
       path: '/photos/albums',
       name: 'Albums',
-      component: () => import('../views/photos/AlbumsView.vue')
+      component: () => import('../views/photos/AlbumsView.vue'),
+      meta: { title: 'Concert and Music Photography | Photo Albums' }
     },
     {
       path: '/photos/gallery/:photosetId',
       name: 'AlbumGallery',
-      component: () => import('../views/photos/GalleryView.vue')
+      component: () => import('../views/photos/GalleryView.vue'),
+      meta: { title: 'Concert and Music Photography | Album Gallery' }
     },
     {
       path: '/music-digital-marketing',
       name: 'Marketing',
-      component: MarketingView
+      component: MarketingView,
+      meta: { title: 'Music Digital Marketing Services' }
     }
   ],
   scrollBehavior(to) {
@@ -52,6 +58,14 @@ const router = createRouter({
     } else {
       return { top: 0, behavior: 'instant' };
     }
+  }
+});
+
+router.beforeEach((to) => {
+  if (to.meta.title) {
+    document.title = `Amalia Cicala | ${to.meta.title}`;
+  } else {
+    document.title = 'Amalia Cicala | Artist Manager, Music Photographer, Music Digital Marketer';
   }
 });
 
